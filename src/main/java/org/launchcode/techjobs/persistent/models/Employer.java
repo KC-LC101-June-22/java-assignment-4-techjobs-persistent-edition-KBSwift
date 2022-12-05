15a07d9275b0c5b7b75824c6bcc7a6106a8fb80a
Kavin Moreno <kavinmoreno@gmail.com>
2022-12-05 05:09:59 -0600

    refactored controllers, added tests

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,38 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "You must enter a location")
+    @Size(min=2, max=100, message = "Please provide between 2 - 100 characters")
+    private String location;
+
+    public Employer (String location){
+        this.location = location;
+    }
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(){}
 
 }
